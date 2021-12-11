441850424061af09e0a4c0cd75e753364e9be79e
Ondrej Spanik <spanik11@msn.com>
2021-12-11 22:39:51 +0100

    Added unoptimized starter point generation

--- __main__.py
+++ __main__.py
@@ -8,12 +8,11 @@ def main():
     # - GENERATOR
     # value for both x,y from center in both positive and negative directions (inclusive)
     field = 5000 
-    # value for both x,y for maximal allowed average distance from the center for clustering
-    # if the average is this value or lower it means clustering success, else failure (inclusive)
-    success = 500
     # number of points with unique coordinates to generate at the beginning randomly
     startsize = 20
     # number of points that will be generated on top of the startsize afterwards
+    # maximal value of these will be used for point generation, others will be derived from the
+    # previously generated points
     sizes = (1000, 2500, 10000, 20000)
     # a randomly selected point will be used after the generation of startsize points
     # for a subsequent generation of sizes[i] points, which shall be generated around such point
@@ -25,12 +24,17 @@ def main():
     # - CLUSTERING
     # k-means count of control points, also its count of clusters
     k = 3
+    # value for both x,y for maximal allowed average distance from the center for clustering
+    # if the average is this value or lower it means clustering success, else failure (inclusive)
+    success = 500
 
     # - VISUALIZATION
 
     # --------------------------------------------------------------------------------------------
 
     # Generate and select points to be used
+    _all = Generator.generatePoints(field, startsize, max(sizes), maxoffset)
+    print(_all)
 
     # Run and time clustering algorithms for every point count
 
--- _generator.py
+++ _generator.py
@@ -1,2 +1,22 @@
+import random
+
 class Generator:
-    pass
+    def generatePoints(fieldsize, startsize, size, maxoffset):
+        # Points that will be generated and returned
+        points = []
+
+        # Starter points (startsize amount)
+        # These points are randomly generated around the field
+        # with unique coordinated
+        # todo make generation less random by guaranteeing that the
+        # todo newly generated points won't be too close to existing ones
+        i = 0
+        while (i != startsize):
+            x = random.randint(-fieldsize, fieldsize)
+            y = random.randint(-fieldsize, fieldsize)
+            starter = [x,y]
+            if (starter not in points):
+                points.append(starter)
+                i += 1
+
+        return points
