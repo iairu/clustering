bcbcf7a2b07d10b22a36937dacba9bffcb3a1a40
Ondrej Spanik <spanik11@msn.com>
2021-12-13 14:53:32 +0100

    Added time graphing with reruns average

--- __main__.py
+++ __main__.py
@@ -5,6 +5,8 @@ from _clusterator import Clusterator
 from datetime import datetime
 from os.path import exists
 
+from time import perf_counter
+
 def main():
     # OPTIONS
     
@@ -21,7 +23,8 @@ def main():
     # number of points that will be generated on top of the startsize afterwards
     # maximal value of these will be used for point generation, others will be derived from the
     # previously generated points
-    sizes = (1000, 2500, 10000, 20000)
+    # sizes = [1000, 2500, 10000, 20000]
+    sizes = [1000]
     # a randomly selected point will be used after the generation of startsize points
     # for a subsequent generation of sizes[i] points, which shall be generated around such point
     # with an offset, whose maximum can be the value below (inclusive)
@@ -36,7 +39,15 @@ def main():
     k = 3
     # value for both x,y for maximal allowed average distance from the center for clustering
     # if the average is this value or lower it means clustering success, else failure (inclusive)
-    success = 500
+    success = 500 # todo implement
+
+
+
+    # - TIMING
+    # time testing rerun count
+    # all will use the same settings mentioned above
+    # difference between runs will occur for random selection of control points and thus time to convergence
+    timing_reruns = 50
 
 
 
@@ -48,12 +59,22 @@ def main():
     exportname = "export/(t)_graph_(s).png"
     # colors for individual clusters, if k > len(colors) then they will loop
     # valid options: https://matplotlib.org/stable/tutorials/colors/colors.html
-    colors = ["r", "g", "b", "m"] 
+    colors = ["r", "g", "b", "m"]
+    # (t) will be replaced with a timestamp and (s) with a size 
+    # if empty then display on screen instead
+    timing_exportname = "export/(t)_time_(s).png"
+    # maximal amount of seconds to plot on graph
+    # this will normalize graph ranges between all sizes
+    # set to 0 for range relative to given single graph's values
+    timing_maxy = 0.01
 
 
 
     # --------------------------------------------------------------------------------------------
 
+
+
+    # - GENERATOR
     # Generate or load all points (point pool) for all algorithms and sizes
     if (exists(cachename)):
         pool = Generator.loadPoints(cachename)
@@ -61,26 +82,80 @@ def main():
         # Pool will have (max(sizes) + startsize) points
         pool = Generator.generatePoints(field, startsize, max(sizes), maxoffset)
         Generator.savePoints(pool, cachename)
-
     # Non-randomly select points to be used depending on sizes
     # Points will exactly match the given size
     selected = []
     for size in sizes:
         selected.append(Generator.modSelectPoints(pool, size))
 
+
+
+    # - CLUSTERING + TIMING
     # Run and time clustering algorithms for every point count
 
+    # Clusters graph data
+    titles = []
+    clusters = []
+
+    # Times graph data
+    alg_range = range(1) # number of active algorithms
+    times = [[[] for alg in alg_range] for size in sizes]
+    times_titles = ["K-Means Centroid wip"]
+
+    for rerun in range(timing_reruns):
+        # For each size (in other words selected size's points)
+        # Run all algorithms, get their output "localclusters" and append them to clusters (all)
+        # Also save titles that include count data and other user constants
+        for i, points in enumerate(selected):
+            alg_pos = 0
+
+            # Run a benchmark for an algorithm
+            time = perf_counter()
+            localclusters = Clusterator.kmeans_centroid(points, k)
+            time = perf_counter() - time
+
+            # Save for timing (all runs considered)
+            times[i][alg_pos].append(time) # times[size][algorithm] = [times for each rerun]
+            alg_pos += 1
+
+            # Save for scatter plot
+            # Only last rerun considered otherwise there would be hundreds of plots
+            if (rerun == timing_reruns - 1):
+                # Create a graph title
+                # Considering modSelectPoints, the sizes should be equivalent 
+                # to user defined sizes, for demonstration use a recounted size
+                recount = sum(len(cluster) for cluster in localclusters)
+                title = "K-Means Centroid wip (k = " + str(k) + ", " + str(recount) + " points)"
+                
+                # Save for graph (only final run considered)
+                clusters.append(localclusters)
+                titles.append(title)
+
+
+
+    # - VISUALIZATION
     # Visualize clusters of points per algorithm incl. times
     # Globally initiate the visualizator with common values for field size, line count and plot colors
-    vis = Visualizator(field, lineevery, colors)
+    vis = Visualizator(field, lineevery, timing_maxy, colors)
 
-    for i, points in enumerate(selected): # todo change selected to clusters after clustering
+    # Clusters graph
+    for i, localclusters in enumerate(clusters):
         # Specify the exportname for the given instance with an up-to-date timestamp and a correct size
-        currname = exportname.replace("(s)", str(sizes[i]))
+        _exportname = exportname.replace("(s)", str(sizes[i]))
         timestamp = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
-        currname = currname.replace("(t)", timestamp)
+        _exportname = _exportname.replace("(t)", timestamp)
         # Then plot all of the clusters for the given instance (algorithm), include a title and export
-        vis.plot([points], "Hello world!", currname)
+        vis.plot(localclusters, titles[i], _exportname)
+
+    # Times graph
+    for i, times_per_size in enumerate(times):
+        # Specify the exportname for the given instance with an up-to-date timestamp and a correct size
+        _exportname = timing_exportname.replace("(s)", str(sizes[i]))
+        timestamp = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+        _exportname = _exportname.replace("(t)", timestamp)
+        # Plot timing
+        vis.time(times_per_size, times_titles, "Algorithm times (k = " + str(k) + ", size = " + str(size) + ")", _exportname)
+
 
 
     return
--- _visualizator.py
+++ _visualizator.py
@@ -1,11 +1,21 @@
 import matplotlib
 from matplotlib import pyplot
+from matplotlib import ticker
+
+def frange(start, step, stop):
+    # float range without numpy
+    fr = []
+    while (start < stop):
+        fr.append(start)
+        start += step
+    return fr
 
 class Visualizator:
-    def __init__(self, field: int, lineevery: int, colors: list):
+    def __init__(self, field: int, lineevery: int, timeMaxY: float, colors: list):
         self.field = field
         self.lineevery = lineevery
         self.colors = colors
+        self.timeMaxY = timeMaxY
         return
 
     # This always creates and outputs a single complete scatter-plot graph from clusters
@@ -35,8 +45,8 @@ class Visualizator:
         ax.set_yticks(range(-field, field + lineevery, lineevery), minor=True) # incl. last label
 
         # Show values on minor lines
-        ax.xaxis.set_minor_formatter('{x: .0f}')
-        ax.yaxis.set_minor_formatter('{x: .0f}')
+        ax.xaxis.set_minor_formatter(ticker.FuncFormatter(lambda x, y: x))
+        ax.yaxis.set_minor_formatter(ticker.FuncFormatter(lambda x, y: x))
 
         # Show minor lines in a slightly transparent color
         ax.grid(which='minor', alpha=0.4)
@@ -64,3 +74,54 @@ class Visualizator:
         else:
             # Show the result on screen
             pyplot.show()
+
+    def time(self, times_per_size: list, alg_titles: list, title: str = "", exportname: str = ""):
+        colors = self.colors
+        maxy = self.timeMaxY
+        export = (exportname != "")
+        
+        # Calculate average times
+        runs = len(times_per_size[0])
+        alg_range = range(len(times_per_size))
+        avgs = [0.0 for alg in alg_range]
+        # Time per size, then algorithm
+        for a, times_per_alg in enumerate(times_per_size):
+            # Add all runs at once and arithmetic average
+            avgs[a] += sum(times_per_alg)
+            avgs[a] /= runs
+        
+        # Hide toolbar for preview window
+        matplotlib.rcParams['toolbar'] = 'None'
+
+        # Create a graph (window) of 5x5 inches
+        fig, ax = pyplot.subplots(figsize=(5,5))
+
+        # Adjust the x,y values to be in frame
+        pyplot.subplots_adjust(left=0.1, bottom=0.05, top=0.95, right=0.99)
+        if (maxy > 0):
+            ax.set(ylim=(0, maxy))
+
+        # Set a graph title
+        ax.set_title(title)
+        ax.set_ylabel("Time per run (sec)")
+        ax.set_xlabel("Runs")
+
+        # Set colors for individual .plot additions
+        ax.set_prop_cycle(color=colors)
+
+        # Add plot data for times per algorithm into the graph
+        for i, times_per_alg in enumerate(times_per_size):
+            ax.plot(range(runs), times_per_alg, linewidth=2.0, label=(alg_titles[i] + " (avg " + "{:.3f}".format(avgs[i]) + ")"))
+
+        # Add a legend showing the labels
+        ax.legend(loc='upper right')
+
+        # Output depending on whether to display on screen or export
+        if (export):
+            # Export the result as a PNG
+            pyplot.savefig(exportname)
+        else:
+            # Show the result on screen
+            pyplot.show()
+
+        return
